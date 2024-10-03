66a93e7be0493d31faba909cf7679dd98e52c72c
hlimhli <144866905+h-lim-hl@users.noreply.github.com>
2024-10-03 12:44:26 +0800

    added weapons get route and replaced hardcode collection name with const variable

--- js/server.js
+++ js/server.js
@@ -19,26 +19,29 @@ async function connectMongoDb (url, dbName) {
 }
 
 async function main() {
+  //Connect to Data Cluster
    const db = await connectMongoDb(MONGODB_URL, dbname);
   app.get('/', (req, res) => {
     res.status(418).json({"message" : "I'm a teapot!"});
   });
 
+  // Get all Materials or subset with query
   app.get('/materials', async (req, res) => {
+    const collectionName = "materials";
     try{
       const { _id, name, density } = req.query;
 
       let query = {};
       if(_id) query["_id"] = new ObjectId(_id);
-      if(name) query["name"]  = name;
+      if(name) query["name"]  = {$regex : name, $options: 'i'};
       if(density) query["density"] = density;
       
       let result;
       if(Object.keys(query).length < 1)
-        result = await db.collection("materials").find().toArray();
+        result = await db.collection(collectionName).find().toArray();
       else {
         console.log(query);
-        result = await db.collection("materials").find(query).toArray();
+        result = await db.collection(collectionName).find(query).toArray();
       }
         res.status(200).json({
         "result" : result
@@ -51,6 +54,36 @@ async function main() {
     }
   });
 
+  // Get all weapons or subset with query
+  app.get('/weapons', async (req, res) => {
+    const collectionName = "weapons";
+    try{
+      const { _id, name, materialName } = req.query;
+
+      let query = {};
+      if(_id) query["_id"] = new ObjectId(_id);
+      if(name) query["name"]  = {$regex : name, $options: 'i'};
+      if(materialName) query["material.name"] = {$regex: materialName, $options: 'i'};
+      
+      let result;
+      if(Object.keys(query).length < 1)
+        result = await db.collection(collectionName).find().toArray();
+      else {
+        console.log(query);
+        result = await db.collection(collectionName).find(query).toArray();
+      }
+        res.status(200).json({
+        "result" : result
+      });
+
+      
+    } catch (error){
+      console.error(`Error in /materials: ${error.toString()}`);
+      res.status(500).json({"error" : "Internal Server Error."});
+    }
+  });
+
+  // Post new weapon unto the weapons collection
   app.post('/weapon', async (req, res) => {
     const { name, material, description } = req.query;
 
@@ -60,8 +93,11 @@ async function main() {
         });
     }
 
+    if(!(await db.collection("materials")))
+      res.status(400).json({"error" : "weapon already exists!"});
+
     const materialDoc = await db.collection("materials")
-      .findOne({ "name": material });
+      .findOne({ "name": {$regex:'^'+name, $options: 'i'} });
 
     if(!materialDoc)
       res.status(400).json({"error" : "material does not exist in database!"})
